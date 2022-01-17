d55e41c8e63432f30748c2e7f8198c709eb37fa4
jhamPac <cloud@kevia.me>
2022-01-17 16:41:29 -0700

    feat: complete fetching projectid via environment file

--- app/Main.hs
+++ app/Main.hs
@@ -1,32 +1,39 @@
-{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE TemplateHaskell #-}
 module Main (main) where
 
-import Import
-import Run
-import RIO.Process
-import Options.Applicative.Simple
-import qualified Paths_blockfrost_excercise
+import           Blockfrost.Client
+import           RIO
+
+xyz :: IO Text
+xyz = do
+  p <- projectFromFile ".env"
+  pure (projectId p)
+
 
 main :: IO ()
 main = do
-  (options, ()) <- simpleOptions
-    $(simpleVersion Paths_blockfrost_excercise.version)
-    "Header for command line arguments"
-    "Program description, also for command line arguments"
-    (Options
-       <$> switch ( long "verbose"
-                 <> short 'v'
-                 <> help "Verbose output?"
-                  )
-    )
-    empty
-  lo <- logOptionsHandle stderr (optionsVerbose options)
-  pc <- mkDefaultProcessContext
-  withLogFunc lo $ \lf ->
-    let app = App
-          { appLogFunc = lf
-          , appProcessContext = pc
-          , appOptions = options
-          }
-     in runRIO app run
+  s <- xyz
+  runSimpleApp $ logInfo $ display s
+
+-- main :: IO ()
+-- main = do
+--   (options, ()) <- simpleOptions
+--     $(simpleVersion Paths_blockfrost_excercise.version)
+--     "Header for command line arguments"
+--     "Program description, also for command line arguments"
+--     (Options
+--        <$> switch ( long "verbose"
+--                  <> short 'v'
+--                  <> help "Verbose output?"
+--                   )
+--     )
+--     empty
+--   lo <- logOptionsHandle stderr (optionsVerbose options)
+--   pc <- mkDefaultProcessContext
+--   withLogFunc lo $ \lf ->
+--     let app = App
+--           { appLogFunc = lf
+--           , appProcessContext = pc
+--           , appOptions = options
+--           }
+--      in runRIO app run
