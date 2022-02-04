b157e8a04e377c5603c0de97f97dcff5b6bf7c61
jhamPac <cloud@kevia.me>
2022-02-03 19:31:21 -0700

    test: simplify tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,1 +1,10 @@
 {-# OPTIONS_GHC -F -pgmF hspec-discover #-}
+
+module Spec where
+import           Test.Hspec
+import           Test.Hspec.QuickCheck
+
+spec :: Spec
+spec = do
+    describe "plus2" $ do
+        it "basic check" $ 2 `shouldBe` 2
