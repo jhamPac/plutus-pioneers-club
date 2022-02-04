8b297af054d8ebedfe4f478625d5f0ee852b6616
jhamPac <cloud@kevia.me>
2022-02-03 19:20:55 -0700

    refactor: use 4 spaces

--- app/Main.hs
+++ app/Main.hs
@@ -6,50 +6,24 @@ import qualified Data.Text         as T
 
 main :: IO ()
 main = do
-  project <- projectFromFile ".env"
-  result <- runBlockfrost project $ do
-    latestBlocks <- getLatestBlock
-    errors <- tryError $ getAccountRewards "Failed"
-    pure (latestBlocks, errors)
+    project <- projectFromFile ".env"
+    result <- runBlockfrost project $ do
+        latestBlocks <- getLatestBlock
+        errors <- tryError $ getAccountRewards "Failed"
+        pure (latestBlocks, errors)
 
-  case result of
-    Left bfe     -> print $ handleError bfe
-    Right (b, _) -> print $ _blockSlotLeader b
+    case result of
+        Left bfe     -> print $ handleError bfe
+        Right (b, _) -> print $ _blockSlotLeader b
 
 
 handleError :: BlockfrostError -> T.Text
 handleError b = case b of
-  BlockfrostError t           -> t
-  BlockfrostBadRequest t      -> t
-  BlockfrostTokenMissing t    -> t
-  BlockfrostFatal t           -> t
-  BlockfrostNotFound          -> "Not found"
-  BlockfrostIPBanned          -> "IP Banned"
-  BlockfrostUsageLimitReached -> "Limit Reached"
-  _                           -> "Client error"
-
-
-
-
--- main :: IO ()
--- main = do
---   (options, ()) <- simpleOptions
---     $(simpleVersion Paths_blockfrost_excercise.version)
---     "Header for command line arguments"
---     "Program description, also for command line arguments"
---     (Options
---        <$> switch ( long "verbose"
---                  <> short 'v'
---                  <> help "Verbose output?"
---                   )
---     )
---     empty
---   lo <- logOptionsHandle stderr (optionsVerbose options)
---   pc <- mkDefaultProcessContext
---   withLogFunc lo $ \lf ->
---     let app = App
---           { appLogFunc = lf
---           , appProcessContext = pc
---           , appOptions = options
---           }
---      in runRIO app run
+    BlockfrostError t           -> t
+    BlockfrostBadRequest t      -> t
+    BlockfrostTokenMissing t    -> t
+    BlockfrostFatal t           -> t
+    BlockfrostNotFound          -> "Not found"
+    BlockfrostIPBanned          -> "IP Banned"
+    BlockfrostUsageLimitReached -> "Limit Reached"
+    _                           -> "Client error"
