a1a44dd7725646d62da70becbc0d43c508aed0f2
jhamPac <cloud@kevia.me>
2022-02-10 19:14:39 -0700

    chore: save point

--- app/Main.hs
+++ app/Main.hs
@@ -8,13 +8,16 @@ main =  do
     project <- projectFromFile ".env"
     result <- runBlockfrost project $ do
                     r <- getAssetsByPolicy "c364930bd612f42e14d156e1c5410511e77f64cab8f2367a9df544d1"
-                    case r of
-                        (_:y:_) -> do
-                            details <- getAssetDetails $ AssetId (_assetInfoAsset y)
-                            case _assetDetailsOnchainMetadata details of
-                                Nothing -> return ("Nah" :: T.Text)
-                                Just d  -> return (_assetOnChainMetadataName d)
-                        _       -> return "Not enough assets"
+                    if fromIntegral (length r) > (1 :: Integer)
+                        then
+                            do
+                                let a = r !! 1
+                                details <- getAssetDetails $ AssetId (_assetInfoAsset a)
+                                case _assetDetailsOnchainMetadata details of
+                                    Nothing -> return ("No asset meta data" :: T.Text)
+                                    Just d  -> return (_assetOnChainMetadataName d)
+
+                        else return "Not enough assets"
 
     case result of
         Left e  -> print e
