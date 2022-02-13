95d8a6bdcc8a8abf70be90c273e8f42e3ae030ab
jhamPac <cloud@kevia.me>
2022-02-12 17:16:51 -0700

    refactor: move code into the Server module

--- app/Main.hs
+++ app/Main.hs
@@ -1,29 +1,6 @@
 module Main (main) where
 
-import           Blockfrost.Client (AssetDetails (_assetDetailsOnchainMetadata),
-                                    AssetId (AssetId),
-                                    AssetInfo (_assetInfoAsset),
-                                    AssetOnChainMetadata (_assetOnChainMetadataName),
-                                    getAssetDetails, getAssetsByPolicy,
-                                    projectFromFile, runBlockfrost)
-import qualified Data.Text         as T
+import qualified Server
 
 main :: IO ()
-main =  do
-    project <- projectFromFile ".env"
-    result <- runBlockfrost project $ do
-                    ats <- getAssetsByPolicy "c364930bd612f42e14d156e1c5410511e77f64cab8f2367a9df544d1"
-                    if length ats > 1
-                        then
-                            do
-                                let asset = ats !! 1
-                                details <- getAssetDetails $ AssetId (_assetInfoAsset asset)
-                                case _assetDetailsOnchainMetadata details of
-                                    Nothing -> return ("No asset meta data" :: T.Text)
-                                    Just d  -> return (_assetOnChainMetadataName d)
-
-                        else return "Not enough assets"
-
-    case result of
-        Left e  -> print e
-        Right t -> print t
+main = Server.run
---
+++ src/Server.hs
@@ -0,0 +1,29 @@
+module Server where
+
+import           Blockfrost.Client (AssetDetails (_assetDetailsOnchainMetadata),
+                                    AssetId (AssetId),
+                                    AssetInfo (_assetInfoAsset),
+                                    AssetOnChainMetadata (_assetOnChainMetadataName),
+                                    getAssetDetails, getAssetsByPolicy,
+                                    projectFromFile, runBlockfrost)
+import qualified Data.Text         as T
+
+run :: IO ()
+run = do
+    project <- projectFromFile ".env"
+    result <- runBlockfrost project $ do
+                    ats <- getAssetsByPolicy "c364930bd612f42e14d156e1c5410511e77f64cab8f2367a9df544d1"
+                    if length ats > 1
+                        then
+                            do
+                                let asset = ats !! 1
+                                details <- getAssetDetails $ AssetId (_assetInfoAsset asset)
+                                case _assetDetailsOnchainMetadata details of
+                                    Nothing -> return ("No asset meta data" :: T.Text)
+                                    Just d  -> return (_assetOnChainMetadataName d)
+
+                        else return "Not enough assets"
+
+    case result of
+        Left e  -> print e
+        Right t -> print t
